c99e3ddf113eef9a2e0eb5dfc1458819520009e2
joseguich <joseguichardomieses@gmail.com>
2023-12-26 21:34:28 -0400

    Todo modificado

--- src/app/places/place.model.ts
+++ src/app/places/place.model.ts
@@ -1,6 +1,6 @@
-export interface Place{
-   id: string;
-   title: string;
-   imageURL: string;
-   comments: string[];
+export interface Place {
+  id: string;
+  title: string;
+  imageURL: string[];
+  comments: string[];
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -9,8 +9,12 @@ export class PlacesService {
     {
       id: '1',
       title: 'Pico Duarte',
-      imageURL:
+      imageURL: [
         'https://i0.wp.com/jibaroenelmundo.com/wp-content/uploads/2019/04/DSCF1022.jpg?fit=1200%2C800&ssl=1',
+        'https://www.ticketswhales.com/wp-content/uploads/2020/11/pico-duarte-tours-and-excursiones.jpg',
+        'https://resources.diariolibre.com/images/2023/08/01/pico-duarte-foto-187bc723-focus-0.01-0.48-608-342.jpg',
+        'https://171614.fs1.hubspotusercontent-na1.net/hub/171614/hubfs/IMG_5262.jpeg?width=650&height=867&name=IMG_5262.jpeg',
+      ],
       comments: [
         'El Pico Duarte, orgullo de la República Dominicana, se alza majestuosamente en la Cordillera Central, marcando el cielo con su cima desafiante a 3,098 metros. Sus laderas están adornadas con bosques nubosos que cobijan una rica biodiversidad, mientras que las aguas cristalinas de sus ríos y arroyos serpentean delicadamente hacia valles verdes y fértiles. Al amanecer, el sol derrama su luz dorada sobre la montaña, revelando caminos serpenteantes que invitan a los aventureros a descubrir sus secretos ocultos entre la neblina matutina y los susurros del viento',
 
@@ -21,8 +25,11 @@ export class PlacesService {
     {
       id: '2',
       title: 'Boca Chica',
-      imageURL:
-        'https://res.cloudinary.com/simpleview/image/upload/v1686632112/clients/dominicanrepublic/Boca_chica_playa_85be931b-bbb6-45a7-a83a-c13f216db446.jpg',
+      imageURL: [
+        'https://cf.bstatic.com/xdata/images/hotel/max1024x768/52941047.jpg?k=cc24436a36bca0d91af78d19d5c84a1c9f43c91dfc6b998ba9ef9d19f42557ec&o=&hp=1',
+        'https://www.colonialtours.com/1parabocachiasamol5422.jpg',
+        'https://www.colonialtours.com/images/juandolioamols3.jpg',
+      ],
       comments: [
         'Boca Chica es una encantadora localidad costera en la República Dominicana, conocida por su vibrante atmósfera y su extensa playa de aguas tranquilas y cristalinas. La arena blanca y suave invita a largas caminatas a orillas del mar Caribe, mientras las palmeras se mecen al ritmo de una brisa suave, ofreciendo sombra refrescante a los visitantes. Es un destino perfecto para quienes buscan relajarse bajo el sol tropical y disfrutar de la hospitalidad cálida de la cultura dominicana.',
 
@@ -51,7 +58,7 @@ export class PlacesService {
   addPlaces(title: string, imageURL: string) {
     this.places.push({
       title,
-      imageURL,
+      imageURL: [],
       comments: [],
       //Generar el id por la longitud o la cantidad, se le suma uno ya que la longitud actual es dos de esta forma agregar otro id y la comillas para convertirlo en string.
       id: this.places.length + 1 + '',
